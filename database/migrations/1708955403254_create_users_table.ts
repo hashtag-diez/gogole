be8cdbdb02c6e8c7192d178086a98dd2b17c37cd
kamaiel <raph.francoise@orange.fr>
2024-02-28 16:26:28 +0100

    First workaround 28/02

--- database/migrations/1708955403254_create_users_table.ts
+++ database/migrations/1708955403254_create_users_table.ts
@@ -1,21 +1,22 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'books'
-
   async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable().unique()
-      table.string('author').notNullable()
+    this.schema.createTable('books', (table) => {
+      table.integer('id').notNullable().unique()
+      table.json('author').notNullable()
       table.string('title').notNullable()
-      table.string('contributors').notNullable()
-      table.string('editor').notNullable()
-      table.string('language').notNullable()
+      table.json("subjects").notNullable()
+      table.string('imagepath').notNullable()
       table.string('filepath').notNullable()
+    })
+
+    this.schema.createTable("book_word_occ", (table) => {
+      table.string('keyword').notNullable()
+      table.integer('book_id').references('id').inTable('books')
+      table.integer('occurrence').notNullable()
     })
   }
 
-  async down() {
-    this.schema.dropTable(this.tableName)
-  }
+  async down() {}
 }
--- start/routes.ts
+++ start/routes.ts
@@ -10,7 +10,13 @@
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => {
-  return {
-    hello: 'world',
+  let res = []
+  for(let i = 0; i<1; i++){
+    const req = await fetch("https://gutendex.com/books/?page=" + (i+1))
+    const data = await req.json()
+    const book_content = await fetch(data.results[0]["formats"]["text/plain; charset=us-ascii"])
+    const data_book = await book_content.text()
+    res = res.concat([data_book])
   }
+  return res
 })
