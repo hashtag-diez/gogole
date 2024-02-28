f069e889b35d24a85eaaeb127584678e1b92da01
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2024-02-28 16:34:11 +0100

    pre-processing first touch

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-import lemmatizer from 'lemmatizer'
+import { lemmatizer } from 'lemmatizer'
 import { removeStopwords } from 'stopword'
 const documentPreProcessing = (documentContent: string) => {
     return lemmatization(filterUnPertinentWords(tokenization(documentContent)))
 }
 
-const lemmatization = (tokens: string[]) => tokens.map((token) => lemmatizer(token))
+const lemmatization = (tokens: string[]) => tokens.map((token) => lemmatizer(token.toLowerCase()))
 
 const filterUnPertinentWords = (tokens: string[]) => removeStopwords(tokens)
 
@@ -13,5 +13,6 @@ const tokenization = (documentContent: string) => {
   return documentContent.split(notAlphabeticCharacter)
 }
 
-const document: string = ""  
-console.log(documentPreProcessing(document))
+const api: string = "https://www.gutenberg.org/cache/epub/1/pg1.txt"
+const request = fetch(api)
+request.then((response) => response.text()).then((data) => console.log(documentPreProcessing(data)))
