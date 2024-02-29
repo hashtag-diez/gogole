000c36bb5e51a0001311b535ab36f71410ee4485
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2024-02-29 09:35:58 +0100

    update test

--- tests/unit/indexation/pre_processing.spec.ts
+++ tests/unit/indexation/pre_processing.spec.ts
@@ -8,11 +8,11 @@ test.group('Indexation pre processing', () => {
     assert.equal(result.includes('Hello'), true)
     assert.equal(result.includes('World'), true)
   })
-  test('word occurence', async ({ assert }) => {
-    const text = 'hello world'
+  test('text pre processing produces tokens and their occurence in the text source', async ({ assert }) => {
+    const text = 'Hello-world. Yes hello everyone!'
     const result = documentPreProcessing(text)
-    assert.equal(result['hello'], 1)
+    assert.equal(result['hello'], 2)
     assert.equal(result['world'], 1)
+    assert.equal(result['everyone'], 1)
   })
 })
-
